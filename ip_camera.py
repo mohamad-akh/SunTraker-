c6d1f9c28110fb149fc322dbdfae27ff13c0231a
mohamad-akh <mohamad.m.akh28@gmail.com>
2026-08-05 19:32:49 +0330

    Replace wrong files with correct files

--- ip_camera.py
+++
@@ -1,3 +0,0 @@
-from core.sun_tracker import SunTracker
-tracker=SunTracker(video_source="http://192.168.1.103:8080/video")
-tracker.run()
--- webcam.py
+++
@@ -1,3 +0,0 @@
-from core.sun_tracker import SunTracker
-tracker=SunTracker(video_source=0)
-tracker.run()
